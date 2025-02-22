e55978d9b46ba50ed85f0aad87f3602dd9adcc28
damjanh <damjan.hudobreznik@gmail.com>
2025-02-22 14:23:28 +0100

    Add EmailsController.

--- SmartCache/Controllers/EmailsController.cs
+++ SmartCache/Controllers/EmailsController.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace SmartCache.Controllers;
 
-public class EmailsController
+[ApiController]
+[Route("emails/{email}")]
+public class EmailsController() : ControllerBase
 {
-    
+    [HttpGet]
+    public IActionResult GetEmail(string email)
+    {
+      return Ok(email); 
+    }
+
+    [HttpPost]
+    public IActionResult PostEmail(string email)
+    {
+        return Created(email, null);
+    }
 }
--- SmartCache/Program.cs
+++ SmartCache/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -9,4 +10,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
