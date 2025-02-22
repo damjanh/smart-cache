236354fdead048eda13ca06e470bd19320ec15c4
damjanh <damjan.hudobreznik@gmail.com>
2025-02-22 14:33:49 +0100

    - Use Orleans and local grain storage.

--- SmartCache/Program.cs
+++ SmartCache/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IEmailsService, OrleansEmailsService>();
 
+builder.Host.UseOrleans(static siloBuilder =>
+{
+    siloBuilder.AddMemoryGrainStorage("emails"); // Locally for now 
+    siloBuilder.UseLocalhostClustering();
+});
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
